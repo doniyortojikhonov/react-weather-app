9bfa409dcc711bd490af6c8f9c3af59c49ca934a
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2023-03-12 20:36:33 +0900

    Production commit

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ const App = () => {
                 className={`${
                     animate ? "animate-shake" : "animate-none"
                 } h-16 bg-black/30 w-full max-w-[450px]
-      rounded-full backdrop-blur-[32px] mb-8`}
+                rounded-full backdrop-blur-[32px] mb-8`}
             >
                 <div className="h-full relative flex items-center justify-between p-2">
                     <input
